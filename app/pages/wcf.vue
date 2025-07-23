b27fb01db0baea1e53adf9c6da718449645bbdb5
steve <snguyen@valleyit.com>
2025-07-23 18:55:22 -0500

    ADD Pricing Page Template

---
+++ app/pages/wcf.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const { data: page } = await useAsyncData("pricing", () =>
+  queryCollection("pricing").first(),
+);
+
+const title = page.value?.seo?.title || page.value?.title;
+const description = page.value?.seo?.description || page.value?.description;
+
+useSeoMeta({
+  title,
+  ogTitle: title,
+  description,
+  ogDescription: description,
+});
+
+defineOgImageComponent("Saas");
+
+const isYearly = ref("0");
+
+const items = ref([
+  {
+    label: "Monthly",
+    value: "0",
+  },
+  {
+    label: "Yearly",
+    value: "1",
+  },
+]);
+</script>
+
+<template>
+
+</template>
