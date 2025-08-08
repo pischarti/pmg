9f3f8588f2fd286f982db886d9112783cf1a82ca
nate <nate@pop-os.lan>
2025-08-08 17:08:05 -0500

    added soteriology and its chapters to the website

--- app/layouts/wcf.vue
+++ app/layouts/wcf.vue
@@ -31,7 +31,19 @@ const navigation = computed(() => {
     },
     'Anthropology': {
       icon: 'i-lucide-user',
-      chapters: ['6', '7', '8', '9']
+      chapters: ['6']
+    }
+    'Anthropology': {
+      icon: 'i-lucide-user',
+      chapters: ['6']
+    }
+  'Anthropology': {
+    icon: 'i-lucide-user',
+    chapters: ['6']
+    }
+  'Soteriology': {
+    icon: 'i-lucide-cross',
+    chapters: ['7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18']
     }
   }
 
